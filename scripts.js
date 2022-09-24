6180a3b4d12395dd977aef919ab9e59b57723066
qfoqs <me@qfoqs.ru>
2022-09-24 14:29:09 +0300

    init

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log('page loaded');
+
+  let btn = document.querySelector('.button');
+  btn.addEventListener('click', () => {
+    console.log('Кнопка нажата');
+
+  })
+
+});
