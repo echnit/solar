3dd4bfc3ccc3a77d4c5917c65945d9160a4b3d2a
qfoqs <me@qfoqs.ru>
2022-11-02 21:57:07 +0300

    Добавляет editorconfig

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("page loaded");
+
+  let btn = document.querySelector(".button");
+  btn.addEventListener("click", () => {
+    console.log("Кнопка нажата");
+  });
+});
